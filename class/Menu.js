8be0374aa56ee6d0d9b1528f7082e0ba8aa202da
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-01-05 18:40:31 +0300

    Initial commit of game

---
+++ class/Menu.js
@@ -0,0 +1,51 @@
+class Menu {
+	constructor() {
+		const readline = require('readline');
+
+		readline.emitKeypressEvents(process.stdin);
+		process.stdin.setRawMode(true);
+
+		process.stdin.on('keypress', (str, key) => key.ctrl && key.name === 'c' && process.exit());
+	}
+
+	clear() {
+		process.stdout.write('\u001b[2J\u001b[0;0H');
+	}
+
+	print() {
+		console.log('***********');
+		console.log('1. Versus human');
+		console.log('2. Versus AI');
+		console.log('3. Exit');
+		console.log('***********');
+	}
+
+	async show() {
+		this.clear();
+		return new Promise((resolve, reject) => {
+			this.print();
+
+			const menu_controller = str => {
+				process.stdin.off('keypress', menu_controller);
+
+				switch (str) {
+					case '1':
+						resolve('human');
+						break;
+					case '2':
+						resolve('ai');
+						break;
+					case '3':
+						reject(0);
+						break;
+					default:
+						process.stdin.on('keypress', menu_controller);
+				}
+			};
+
+			process.stdin.on('keypress', menu_controller);
+		});
+	}
+}
+
+module.exports = Menu;
---
+++ main.js
@@ -0,0 +1,31 @@
+/*
+    1. Main menu (select mode - PVP or PVE)
+    2. Game
+        2.1. Board
+        2.2. Players
+        2.3. Turn-based mechanic
+        2.4. Checker for conditions
+        2.5. AI
+*/
+
+const Menu = require('./class/Menu');
+
+class Game {
+	constructor() {
+		this.menu = new Menu();
+	}
+
+	play() {
+		this.menu
+			.show()
+			.then(mode => {
+				
+			})
+			.catch(status => {
+				process.exit(status);
+			});
+	}
+}
+
+const main_game = new Game();
+main_game.play();
