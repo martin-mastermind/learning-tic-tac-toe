d0d02e83d7d244f90f3765dc29a161bbb526ee43
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-01-05 19:26:09 +0300

    Implement players on AI-mode

--- class/Gameplay.js
+++ class/Gameplay.js
@@ -1,14 +1,26 @@
 const Board = require('./Board');
+const Player = require('./Player');
 
 class Gameplay {
 	constructor(mode) {
 		this.mode = mode;
 		this.board = new Board(3);
+		this.players = {};
+
+		if (mode === 'human') {
+		} else {
+			const is_human_first = !!Math.round(Math.random());
+			this.players.x = new Player(is_human_first, 'x');
+			this.players.o = new Player(!is_human_first, 'o');
+			this.turn = is_human_first ? 'x' : 'o';
+		}
+
 		this.init();
 	}
 
 	init() {
 		this.board.render();
+		this.players[this.turn].listen();
 	}
 }
 
--- class/Player.js
+++ class/Player.js
@@ -1,5 +1,24 @@
 class Player {
-    
+	/**
+	 * @param {Boolean} isHuman determine whether player is human or AI
+	 * @param {String} figure which figure player is
+	 */
+	constructor(isHuman, figure) {
+		this.isHuman = isHuman;
+		this.figure = figure;
+	}
+
+	off() {
+		process.stdin.off('keypress', this.move);
+	}
+
+	move(str) {
+		console.log(str);
+	}
+
+	listen() {
+		process.stdin.on('keypress', this.move);
+	}
 }
 
 module.exports = Player;
