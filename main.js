df4b6ff9cc37bab307ba02568438f6e003c79226
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-01-06 15:09:37 +0300

    Remove todo

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-/*
-    V1. Main menu (select mode - PVP or PVE)
-    2. Game
-        V2.1. Board
-        V2.2. Players
-        V2.3. Turn-based mechanic
-        2.4. Checker for conditions
-        2.5. AI
-*/
-
 const Menu = require('./class/Menu');
 const Gameplay = require('./class/Gameplay');
 
